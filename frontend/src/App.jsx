fa2954f396169214cd6758965988552e63f36af9
Vidhi <vidhiichauhan7@gmail.com>
2025-09-11 00:47:34 +0530

    Initial commit of the project.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css'
+import Navbar from './components/ui/Navbar'
+import Hero from './components/ui/Herosection'
+import CoworkingPage from './components/ui/CoworkingPage';
+import OfficeSpacePage from './components/ui/OfficeSpacePage';
+import EventsPage from './components/ui/EventsPage';
+import PrivateOfficePage from './components/ui/PrivateOfficePage';
+import CityDetailPage from './components/ui/CityDetailPage';
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <Router>
+      <div className="font-sans">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/coworking/:city" element={<CoworkingPage />} />
+          <Route path="/office-spaces/:city" element={<OfficeSpacePage />} />
+           <Route path="/events-shoots-and-advertising/:city" element={<EventsPage />} />
+           <Route path="/private-office/:city" element={<PrivateOfficePage />} /> 
+           <Route path="/centre/:city" element={<CityDetailPage />} />
+
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+export default App
